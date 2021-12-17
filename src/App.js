43dd0a91a0807045a9b51efa33786a7d0fe26eca
Walim <machine-hb@hotmail.fr>
2021-12-17 21:25:55 +0100

    Launch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,112 @@
-
-import Hero from './components/Hero';
-import './style/style.css';
+import Hero from "./components/Hero";
+import "./style/style.scss";
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
-    <Hero />
+    <>
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul>
+          <li>
+            <a href="">À propos</a>
+          </li>
+          <li>
+            <a href="">Sponsors</a>
+          </li>
+          <li>
+            <a href="">Agenda</a>
+          </li>
+          <li>
+            <a href="">Contenu</a>
+          </li>
+        </ul>
+      </nav>
+      <Hero />
+
+      <section className="about">
+        <h1>01</h1>
+        <h2>La conference</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+          amet, consetetur sadipscing elitr, sed diam
+        </p>
+        <div className="btn__video">
+          <i></i>
+          <p>Watch video</p>
+        </div>
+      </section>
+
+      <section className="sponsor">
+        <h1>01</h1>
+        <h2>La conference</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+          amet, consetetur sadipscing elitr, sed diam
+        </p>
+        <div className="sponsor__card">
+          {
+            //Images
+          }
+        </div>
+      </section>
+
+      <section className="agenda">
+        <h1>01</h1>
+        <h2>La conference</h2>
+        <h3>JEUDI 13 JANVIER 2022</h3>
+        <div className="line__active"></div>
+        <div className="plan">
+          <div className="plan__component">
+            <div className="plan__hour"></div>
+            <div className="plan__description">
+              <div className="description__title"></div>
+              <div className="description__descript"></div>
+            </div>
+          </div>
+          <div className="plan__line"></div>
+        </div>
+      </section>
+
+      <section className="whitepaper">
+        <h1>01</h1>
+        <h2>La conference</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        </p>
+        <div className="whitepaper__card">
+          {
+            //image
+          }
+          <h4>Title</h4>
+          <div className="btn__download">
+            <i></i>
+            <p>Télécharger le contenu</p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,48 @@
-import React, {useEffect, useRef} from "react";
-import logo from "../assets/logo.svg";
+import React, { useEffect, useRef } from "react";
 import backgroundVideo from "../assets/video.mp4";
-import gsap, {Power3} from "gsap";
+import gsap, { Power3 } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Hero() {
+  // References
+  //const sus = useRef(null);
+  let tl = useRef(null);
 
-    // References
-    const sus = useRef(null);
-
-    // Render
-    useEffect(() => {
-        
-        gsap.to(sus.current, {
+  // Render
+  useEffect(() => {
+    /*   
+      tl.current = gsap.timeline()
+      .to(sus.current, {
             y: -290,
             x: -500,
             scale: .2,
             ease: Power3.easeOut
-        }, 1.5)
+        }, .9)
 
-    })
+        ScrollTrigger.create({
+          trigger: ".hero",
+          animation: tl.current,
+          markers: true, // reference
+          start: "top",
+          end: "center",
+          scrub: 1,
+          toggleActions:"play complete reverse reset",
+          pin: true
+        });
+*/
+  });
   return (
     <div class="hero">
-      <img src={logo} alt=""  ref={sus}/>
+      <h1>CONCEVONS ENSEMBLE DES VILLES INTELLIGENTES ET DURABLES</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem
+      </p>
       <video autoPlay loop muted id="video">
         <source src={backgroundVideo} type="video/mp4" />
       </video>
