312e4cbd8b43bdd16cfbf2158572e5ba01bcdc8d
Walim <machine-hb@hotmail.fr>
2021-12-18 14:24:05 +0100

    About page

--- src/App.js
+++ src/App.js
@@ -1,49 +1,64 @@
 import Hero from "./components/Hero";
 import "./style/style.scss";
-import logo from "./assets/logo.svg";
+import aboutimg from "./assets/aboutimg.svg";
 
 function App() {
   return (
     <>
-      <nav>
-        <div className="logo">
-          <img src={logo} alt="" />
-        </div>
-        <ul>
-          <li>
-            <a href="">À propos</a>
-          </li>
-          <li>
-            <a href="">Sponsors</a>
-          </li>
-          <li>
-            <a href="">Agenda</a>
-          </li>
-          <li>
-            <a href="">Contenu</a>
-          </li>
-        </ul>
-      </nav>
       <Hero />
 
       <section className="about">
-        <h1>01</h1>
-        <h2>La conference</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-          amet, consetetur sadipscing elitr, sed diam
-        </p>
-        <div className="btn__video">
-          <i></i>
-          <p>Watch video</p>
+        <div className="container">
+          <div className="about__text">
+            <h1>01</h1>
+            <h2>La conference</h2>
+            <p className="about__description">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam
+              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+              dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+              elitr, sed diam
+            </p>
+            <div className="btn__video">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                height="22"
+                viewBox="0 0 28 22"
+              >
+                <g id="icons8_video" transform="translate(-1.654 -4)">
+                  <path
+                    id="Tracé_45"
+                    data-name="Tracé 45"
+                    d="M18.029,12.356,11.1,8.2A1.386,1.386,0,0,0,9,9.386V17.7a1.386,1.386,0,0,0,2.1,1.189l6.93-4.158a1.386,1.386,0,0,0,0-2.377Z"
+                    transform="translate(2.701 1.544)"
+                    fill="#abdf75"
+                  />
+                  <rect
+                    id="Rectangle_10"
+                    data-name="Rectangle 10"
+                    width="28"
+                    height="22"
+                    rx="3"
+                    transform="translate(1.654 4)"
+                    fill="#abdf75"
+                    opacity="0.35"
+                  />
+                </g>
+              </svg>
+
+              <p className="btn__label">Watch video</p>
+            </div>
+          </div>
+          <div className="image">
+            <img src={aboutimg} alt="" />
+          </div>
         </div>
       </section>
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import backgroundVideo from "../assets/video.mp4";
 import gsap, { Power3 } from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import logo from "../assets/logo.svg";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -34,18 +35,77 @@ export default function Hero() {
 */
   });
   return (
-    <div class="hero">
-      <h1>CONCEVONS ENSEMBLE DES VILLES INTELLIGENTES ET DURABLES</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem
-      </p>
-      <video autoPlay loop muted id="video">
-        <source src={backgroundVideo} type="video/mp4" />
-      </video>
-    </div>
+    <>
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul>
+          <li>
+            <a href="">À propos</a>
+          </li>
+          <li>
+            <a href="">Sponsors</a>
+          </li>
+          <li>
+            <a href="">Agenda</a>
+          </li>
+          <li>
+            <a href="">Contenu</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="hero">
+        <div className="content">
+          <h1>CONCEVONS ENSEMBLE DES VILLES INTELLIGENTES ET DURABLES</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo
+          </p>
+        </div>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="44"
+            viewBox="0 0 32 44"
+          >
+            <g
+              id="Groupe_15"
+              data-name="Groupe 15"
+              transform="translate(-664 -648)"
+            >
+              <g
+                id="Tracé_29"
+                data-name="Tracé 29"
+                transform="translate(664 648)"
+                fill="none"
+              >
+                <path
+                  d="M16,0A16,16,0,0,1,32,16V28A16,16,0,0,1,0,28V16A16,16,0,0,1,16,0Z"
+                  stroke="none"
+                />
+                <path
+                  d="M 16 3 C 12.5275707244873 3 9.262990951538086 4.352241516113281 6.807609558105469 6.807609558105469 C 4.352239608764648 9.262989044189453 3 12.5275707244873 3 16 L 3 28 C 3 31.47241973876953 4.352239608764648 34.73701095581055 6.807609558105469 37.19239044189453 C 9.262990951538086 39.64775848388672 12.5275707244873 41 16 41 C 19.47243118286133 41 22.73701095581055 39.64775848388672 25.19239044189453 37.19239044189453 C 27.64776039123535 34.73701095581055 29 31.47241973876953 29 28 L 29 16 C 29 12.5275707244873 27.64776039123535 9.262989044189453 25.19239044189453 6.807609558105469 C 22.73701095581055 4.352241516113281 19.47243118286133 3 16 3 M 16 0 C 24.8365592956543 0 32 7.163440704345703 32 16 L 32 28 C 32 36.83655166625977 24.8365592956543 44 16 44 C 7.163440704345703 44 0 36.83655166625977 0 28 L 0 16 C 0 7.163440704345703 7.163440704345703 0 16 0 Z"
+                  stroke="none"
+                  fill="#f4f4f4"
+                />
+              </g>
+              <circle
+                id="Ellipse_1"
+                data-name="Ellipse 1"
+                cx="3"
+                cy="3"
+                r="3"
+                transform="translate(677 659)"
+                fill="#fff"
+              />
+            </g>
+          </svg>
+        <video autoPlay loop muted id="video">
+          <source src={backgroundVideo} type="video/mp4" />
+        </video>
+      </div>
+    </>
   );
 }
